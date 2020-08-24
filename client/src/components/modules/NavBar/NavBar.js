8f5f12bdc2975723036f66bb9d86dbf46e1a32bd
Kevin Jiang <kev2018@mit.edu>
2020-08-23 20:23:13 -0400

    Add NavBar Component

---
+++ client/src/components/modules/NavBar/NavBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './NavBar.css';
+import { NavLink } from 'react-router-dom'
+import logo from './CSOP Logo.png';
+
+
+class NavBar extends React.Component {
+    render() {
+        return (
+            <div className="NavBar">
+                <NavLink to="/"> 
+                    <img className="Logo" src={logo}/>
+                </NavLink>
+                <nav>
+                    <ul className="NavLinks">
+                        <li><NavLink to="/dashboard" className="DashboardLink"> Dashboard </NavLink></li>
+                        <li><NavLink to="/about" className="AboutLink"> About </NavLink></li>
+                        <li><NavLink to="/leaderboard" className="LeaderboardLink"> Leaderboard </NavLink></li>
+                    </ul>
+                </nav>
+                <button className="Login">Login</button>
+            </div>
+        );
+    }
+}
+
+export default NavBar;
