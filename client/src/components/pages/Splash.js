b627bf5992bdc8b0782a363a478df8f9b6db924c
Kevin Jiang <kev2018@mit.edu>
2020-08-30 20:26:06 -0400

    Add Background Chips

--- client/src/components/pages/Splash.js
+++ client/src/components/pages/Splash.js
@@ -4,6 +4,9 @@ import "./Pages.css";
 import "./Splash.css";
 import miniChips from "./mini-chips.png";
 import bigChip from "./csop3d-32.png";
+import leftChip from "./left-chip.png";
+import leftChipDark from "./left-chip-2.png";
+import trophy from "./Trophy.png";
 
 class Splash extends Component {
   constructor(props) {
@@ -19,7 +22,7 @@ class Splash extends Component {
   render() {
     console.log("Loaded splash")
     return (
-      <div className = "content-container">
+      <div className="content-container">
         <div className="Content">
           <img className="MiniChips" src={miniChips}/>
           <h1 className="Title">Collegiate Series of Poker</h1>
@@ -28,6 +31,8 @@ class Splash extends Component {
             <button className="Play">Play Now</button>
           </NavLink>
         </div>
+        <img className="LeftChipLight" src={leftChip}/>
+        <img className="LeftChipDark" src={leftChipDark}/>
         <img className="BigChip" src={bigChip}/>
       </div>
     );
