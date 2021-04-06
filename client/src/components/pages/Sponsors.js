b210654c14332a9a5aed83499f5af92d15016c24
Daniel Sun <dansun@mit.edu>
2021-04-05 23:16:32 -0400

    update FAQ

---
+++ client/src/components/pages/Sponsors.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+import "./Pages.css";
+class Sponsors extends Component {
+    constructor(props) {
+        super(props);
+        // Initialize Default State
+        this.state = {};
+    }
+
+    componentDidMount() {
+        // remember -- api calls go here!
+    }
+
+    render() {
+        return (
+            <div><div >Sponsors</div>
+                <div >Sponsors</div>
+                <div >Sponsors</div>
+                <div >Sponsors</div>
+                <div >Sponsors</div>
+                <div >Sponsors</div>
+                <div >Sponsors</div>
+            </div>
+
+        )
+    }
+}
+
+export default Sponsors;
