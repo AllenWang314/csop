a9eb4beaa15e174942020c80feee4dc8d576279f
Allen Wang <abmathcounts@gmail.com>
2021-05-02 01:49:37 -0400

    added facebook and instagram links to contact

--- client/src/components/pages/Contact.js
+++ client/src/components/pages/Contact.js
@@ -19,7 +19,11 @@ class Contact extends Component {
       <div className = "content-container">
           <h1 className="Title">Contact</h1>
           <p className="contact-body">
-            Please email <a href="mailto:official.csop@gmail.com">official.csop@gmail.com</a> for inquries regarding logistics, registration, or sponsorships. We typically respond within 24 hours and are always happy to answer any questions that you may have!
+            Please email <a href="mailto:official.csop@gmail.com">official.csop@gmail.com</a> for inquries regarding logistics, registration, or sponsorships. We typically respond within 24 hours and are always happy to answer any questions that you may have.
+          </p>
+
+          <p className="contact-body">
+            Follow us on instagram <a href="https://www.instagram.com/pokercsop/">@pokercsop</a> or find us on <a href="https://www.facebook.com/PokerCSOP">facebook</a>!
           </p>
       </div>
     );
